56928b03e61efa66874c47d6921770233a82a274
guoshengfang <15088138492@163.com>
2019-11-14 14:50:01 +0800

    springboot项目可以在tomcat中运行

--- jenkins-demo-20191112/src/main/java/com/gsf/jenkinsdemo20191112/JenkinsDemo20191112Application.java
+++ jenkins-demo-20191112/src/main/java/com/gsf/jenkinsdemo20191112/JenkinsDemo20191112Application.java
@@ -2,9 +2,16 @@ package com.gsf.jenkinsdemo20191112;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsDemo20191112Application {
+public class JenkinsDemo20191112Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinsDemo20191112Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemo20191112Application.class, args);
